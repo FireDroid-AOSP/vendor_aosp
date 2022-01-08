2d42daea9a93f86d407305340c7441c22fb95681
SagarMakhar <sagarmakhar@gmail.com>
2022-01-08 18:49:07 +0000

    vendor: Add Black theme [3/3]
    Change-Id: I362c75a9a9456bb4d7ffe12072b1b2ef1ea49dac
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -207,6 +207,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/aosp/overlay/common
 
 PRODUCT_PACKAGES += \
+    AndroidBlackThemeOverlay \
     CustomFontPixelLauncherOverlay \
     DocumentsUIOverlay \
     NetworkStackOverlay \
