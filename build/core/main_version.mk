9da41f30e1cbd9351225e86a4935d63e475e1d42
1RONAN1 <rdfgaming2019@gmail.com>
2023-11-18 23:35:09 +0530

    vendor: Rebranded to FireDroid !!

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -4,22 +4,12 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
 endif
 
-# LineageOS System Version
+# Versioning Props
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
-    ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
-    ro.modversion=$(LINEAGE_VERSION) \
-    ro.lineagelegal.url=https://lineageos.org/legal
+    ro.custom.build.date=$(BUILD_DATE) \
+    ro.custom.device=$(CUSTOM_BUILD) \
+    ro.custom.fingerprint=$(ROM_FINGERPRINT) \
+    ro.custom.version=$(CUSTOM_VERSION) \
+    ro.modversion=$(CUSTOM_VERSION)
 
-# LineageOS Platform Display Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION)
 
-# LineageOS Platform SDK Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
-
-# LineageOS Platform Internal Version
-ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
---
+++ config/branding.mk
@@ -0,0 +1,22 @@
+# Type of Build
+CUSTOM_BUILD_TYPE ?= UNOFFICIAL
+
+# FireDroid Props
+FireDroid_BASE_VERSION = v2.0-Hellix
+
+# Time Related
+CUSTOM_DATE_YEAR := $(shell date -u +%Y)
+CUSTOM_DATE_MONTH := $(shell date -u +%m)
+CUSTOM_DATE_DAY := $(shell date -u +%d)
+CUSTOM_DATE_HOUR := $(shell date -u +%H)
+CUSTOM_DATE_MINUTE := $(shell date -u +%M)
+CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONTH)-$(CUSTOM_DATE_DAY) $(CUSTOM_DATE_HOUR):$(CUSTOM_DATE_MINUTE) UTC' +%s)
+CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
+
+# Platform (Android Version)
+CUSTOM_PLATFORM_VERSION := 14.0
+FireDroid_BUILD_VERSION = 14.0
+
+# Custom Versioning
+CUSTOM_VERSION := FireDroid-$(FireDroid_BASE_VERSION)-$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
+CUSTOM_VERSION_PROP := fourteen
