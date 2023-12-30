PixysOS/vendor_pixys

vendor: import fonts from SPB5.210812.002
Change-Id: I2913ab4271a9f591d002000405bf501aca8846cf
Signed-off-by: aswin7469 <aswinas@pixysos.com>

config/common.mk
@@ -97,6 +97,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Bootanimation
 $(call inherit-product, vendor/pixys/bootanimation/bootanimation.mk)
 
+# Fonts
+$(call inherit-product, vendor/pixys/config/fonts.mk)
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig

config/fonts.mk
@@ -1,0 +1,59 @@
+# arbutus-slab family
+PRODUCT_PACKAGES += \
+    ArbutusSlab-Regular.ttf
+
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf
+
+# Lustria family
+PRODUCT_PACKAGES += \
+    Lustria-Regular.ttf
+
+# lato family
+PRODUCT_PACKAGES += \
+    Lato-BoldItalic.ttf \
+    Lato-Bold.ttf \
+    Lato-Italic.ttf \
+    Lato-MediumItalic.ttf \
+    Lato-Medium.ttf \
+    Lato-Regular.ttf
+
+# rubik family
+PRODUCT_PACKAGES += \
+    Rubik-BoldItalic.ttf \
+    Rubik-Bold.ttf \
+    Rubik-Italic.ttf \
+    Rubik-MediumItalic.ttf \
+    Rubik-Medium.ttf \
+    Rubik-Regular.ttf
+
+# zilla-slab family
+PRODUCT_PACKAGES += \
+    ZillaSlab-MediumItalic.ttf \
+    ZillaSlab-Medium.ttf \
+    ZillaSlab-SemiBoldItalic.ttf \
+    ZillaSlab-SemiBold.ttf
+
+# Karla family
+PRODUCT_PACKAGES += \
+    Karla-Regular.ttf
+
+# Fraunces family
+PRODUCT_PACKAGES += \
+    Fraunces-Regular.ttf \
+    Fraunces-SemiBold.ttf
+
+# BigShouldersText family
+PRODUCT_PACKAGES += \
+    BigShouldersText-Bold.ttf \
+    BigShouldersText-ExtraBold.ttf
+
+# Barlow family
+PRODUCT_PACKAGES += \
+    Barlow-Bold.ttf \
+    Barlow-Medium.ttf
+
+PRODUCT_COPY_FILES += \
+    vendor/pixys/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml

FireDroid-AOSP/vendor_aosp

config/common.mk
@@ -127,6 +127,12 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Bootanimation
 include vendor/aosp/config/bootanimation.mk
 
+# Fonts
+$(call inherit-product, vendor/aosp/config/fonts.mk)
+
+# Branding
+include vendor/aosp/config/branding.mk
+
 # BtHelper
 PRODUCT_PACKAGES += \
     BtHelper

config/fonts.mk
@@ -1,0 +1,59 @@
+# arbutus-slab family
+PRODUCT_PACKAGES += \
+    ArbutusSlab-Regular.ttf
+
+# google-sans family
+PRODUCT_PACKAGES += \
+    GoogleSans-Italic.ttf \
+    GoogleSans-Regular.ttf
+
+# Lustria family
+PRODUCT_PACKAGES += \
+    Lustria-Regular.ttf
+
+# lato family
+PRODUCT_PACKAGES += \
+    Lato-BoldItalic.ttf \
+    Lato-Bold.ttf \
+    Lato-Italic.ttf \
+    Lato-MediumItalic.ttf \
+    Lato-Medium.ttf \
+    Lato-Regular.ttf
+
+# rubik family
+PRODUCT_PACKAGES += \
+    Rubik-BoldItalic.ttf \
+    Rubik-Bold.ttf \
+    Rubik-Italic.ttf \
+    Rubik-MediumItalic.ttf \
+    Rubik-Medium.ttf \
+    Rubik-Regular.ttf
+
+# zilla-slab family
+PRODUCT_PACKAGES += \
+    ZillaSlab-MediumItalic.ttf \
+    ZillaSlab-Medium.ttf \
+    ZillaSlab-SemiBoldItalic.ttf \
+    ZillaSlab-SemiBold.ttf
+
+# Karla family
+PRODUCT_PACKAGES += \
+    Karla-Regular.ttf
+
+# Fraunces family
+PRODUCT_PACKAGES += \
+    Fraunces-Regular.ttf \
+    Fraunces-SemiBold.ttf
+
+# BigShouldersText family
+PRODUCT_PACKAGES += \
+    BigShouldersText-Bold.ttf \
+    BigShouldersText-ExtraBold.ttf
+
+# Barlow family
+PRODUCT_PACKAGES += \
+    Barlow-Bold.ttf \
+    Barlow-Medium.ttf
+
+PRODUCT_COPY_FILES += \
+    vendor/aosp/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml