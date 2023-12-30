1204957b459bfddeea60623e17d4ae04ff64283c
1RONAN1 <rdfgaming2019@gmail.com>
2023-12-31 02:57:56 +0530

    aosp: add support for vimusic

--- config/common.mk
+++ config/common.mk
@@ -85,6 +85,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
 endif
 
+#ViMusic
+PRODUCT_PACKAGES += \
+        ViMusic
+
 # Enforce privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
