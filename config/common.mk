PixysOS/vendor_pixys

vendor: import fonts from SPB5.210812.002
Change-Id: I2913ab4271a9f591d002000405bf501aca8846cf
Signed-off-by: aswin7469 <aswinas@pixysos.com>

config/common.mk
@@ -97,6 +97,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 # Bootanimation
 $(call inherit-product, vendor/pixys/bootanimation/bootanimation.mk)
 
+# Fonts
+$(call inherit-product, vendor/pixys/config/fonts.mk)
+
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig

FireDroid-AOSP/vendor_aosp

config/common.mk
@@ -139,6 +139,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/aosp/prebuilt/common/etc/init/init.custom-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.custom-updater.rc \
 
+# Fonts
+$(call inherit-product, vendor/aosp/config/fonts.mk)
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images \